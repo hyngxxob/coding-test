6257ddf94e57bc99380a18c605401abf0ebf5e46
Hyungseob Kim <supremepilot@naver.com>
2022-07-11 18:45:38 +0900

    push

--- C#/모의고사/practice.cs
+++ C#/모의고사/practice.cs
@@ -8,7 +8,7 @@ public class Solution {
         int[] std2 = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
         int[] std3 = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
         int[] hit = new int[3];
-        int test = 0;
+        
         for (int i = 0; i < answers.Length; i++)
         {
             if (answers[i] == std1[i % std1.Length]) ++hit[0];
